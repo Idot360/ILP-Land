ef4719d727f966d731a63809ccda1e9ab9afb17b
Idot360 <evan.h@outlook.com>
2024-08-02 12:23:00 +0800

    partial success

--- fitting.py
+++ fitting.py
@@ -2,8 +2,12 @@
 
 def train(file, features):
     import pandas as pd
-    from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+    from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
     from sklearn import ensemble
+    from sklearn.preprocessing import PolynomialFeatures
+    from sklearn.pipeline import Pipeline
+    from sklearn.linear_model import LinearRegression
+    import numpy as np
 
     training_data = pd.read_csv(file, index_col='ID')
     training_data = training_data.dropna(axis=0)
@@ -14,8 +18,12 @@ def train(file, features):
     #clf = QuadraticDiscriminantAnalysis()
     #clf.fit(X, y)
 
-    model = ensemble.RandomForestRegressor(random_state=2024, n_estimators=200)
-    model.fit(X, y)
+    model = Pipeline([('poly', PolynomialFeatures(degree=3)),
+                    ('linear', LinearRegression(fit_intercept=False))])
+    model = model.fit(X, y)
+
+    #model = ensemble.RandomForestRegressor(random_state=2024, n_estimators=200)
+    #model.fit(X, y)
 
     return model
     
@@ -24,7 +32,7 @@ def train(file, features):
 def main():
     
     file = 'processed_data.csv'
-    features = ["Population","Land size","Working hours","Environment","Income","Cost of living","N recreational buildings","N office buildings","N essential buildings","N residential buildings"]
+    features = ["Population","Land size","Working hours","Environment","Affordability","N recreational buildings","N office buildings","N essential buildings","N residential buildings"]
 
     model = train(file, features)
 
--- optimization.py
+++ optimization.py
@@ -1,46 +1,57 @@
 
-def f(x1, x2, x3, x4):
+import scipy as sp
+from scipy.optimize import LinearConstraint
+from scipy.optimize import minimize
+
+import fitting
+
+
+def f(x):
+    import numpy as np
+
     #offset = np.random.normal(0, 10, 4)
     #parameters = np.array([x1,x2,x3,x4]) + offset
     
-    c1, c2, c3, c4, c5, c6, c7 = CONSTANTS
-    return MODEL.predict([c1,c2,c3,c4,c5,c6,c7,x1,x2,x3,x4])
+    c1, c2, c3, c4, c5 = CONSTANTS
+    x1, x2, x3, x4 = x
+    return -1*MODEL.predict(np.array([c1, c2, c3, c4, c5, x1, x2, x3, x4]).reshape(1,-1))
 
 
 
 def main(feature_constants):
-    import scipy as sp
-    from scipy.optimize import LinearConstraint
-    from scipy.optimize import minimize
-
-    import fitting
-
     global CONSTANTS
     CONSTANTS = feature_constants
+
+    CONSTANTS[3] *=100
+    CONSTANTS[2] *=10
+    CONSTANTS[0] *=100
+    CONSTANTS.append(CONSTANTS[4]- CONSTANTS[5])
+    CONSTANTS.pop(5)
+    CONSTANTS.pop(4)
+    CONSTANTS[0] = int(CONSTANTS[0])
+    CONSTANTS[2] = int(CONSTANTS[2])
+    print(CONSTANTS)
 
     global MODEL
     MODEL = fitting.main()
 
 
-    buildings_max = CONSTANTS[1]/0.005
+    buildings_max = CONSTANTS[1]/0.05
 
     #cons = ({'type': 'ineq', 'fun': lambda x:  x[0] - 2 * x[1] + 2},
     #    {'type': 'ineq', 'fun': lambda x: -x[0] - 2 * x[1] + 6},
     #    {'type': 'ineq', 'fun': lambda x: -x[0] - 2 * x[1] + 6},
     #    {'type': 'ineq', 'fun': lambda x: -x[0] + 2 * x[1] + 2})
     cons = ()
-    
-    bnds = ((1, buildings_max), (1, buildings_max), (1, buildings_max), (1, buildings_max))
+        
+    bnds = ((50, buildings_max), (50, buildings_max), (50, buildings_max), (50, buildings_max))
 
-    print(f)
-
-    return minimize(f, [15000, 5, 5, 5], bounds=bnds, constraints=cons)
+    #minimize(f, [15000, 5000, 5000, 5000], method='BFGS', options={'gtol': 1e-1, 'disp': True})
+    return minimize(f, [100, 500, 500, 500], bounds=bnds, constraints=cons, method='SLSQP', options={"disp":True})
 
 
 if __name__ == "__main__":
     import sys
-    #main(sys.argv[1:])
-    main([2.7,223,31.6,2,6,96000,83600,5000,3500,1430,1600])
-
+    main(sys.argv[1:])
 
 
